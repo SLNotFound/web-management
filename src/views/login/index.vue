c88b0444686215675ce4029533e414fc22f9e531
sunlei <1255193087@qq.com>
2023-10-07 21:50:09 +0800

    登录页

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -5,13 +5,69 @@
       <h1>登录</h1>
       <el-card shadow="never" class="login-card">
         <!--登录表单-->
+        <el-form ref="form" :model="loginForm" :rules="loginRules">
+          <el-form-item prop="mobile">
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号" />
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" show-password placeholder="请输入密码" />
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="loginForm.isAgree">
+              用户平台使用协议
+            </el-checkbox>
+          </el-form-item>
+          <el-form-item>
+            <el-button style="width: 350px" type="primary" @click="login">登录</el-button>
+          </el-form-item>
+        </el-form>
       </el-card>
     </div>
   </div>
 </template>
 <script>
 export default {
-  name: 'Login'
+  name: 'Login',
+  data() {
+    return {
+      loginForm: {
+        mobile: '',
+        password: '',
+        isAgree: false
+      },
+      loginRules: {
+        mobile: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          pattern: /^1[3-9]\d{9}$/,
+          message: '手机号格式不正确',
+          trigger: 'blur'
+        }],
+        password: [{
+          required: true,
+          message: '请输入手机号',
+          trigger: 'blur'
+        }, {
+          min: 6,
+          max: 16,
+          message: '密码长度应该为6-16之间',
+          trigger: 'blur'
+        }],
+        isAgree: [{
+          validator: (rule, value, callback) => {
+            value ? callback() : callback(new Error('没有勾选用户平台协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login() {
+      console.log('登录')
+    }
+  }
 }
 </script>
 <style lang="scss">

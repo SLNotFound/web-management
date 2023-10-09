ed72647736a4500c243b85690378698e7bf31d81
sunlei <1255193087@qq.com>
2023-10-09 10:55:20 +0800

    退出

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -16,17 +16,18 @@
         <el-dropdown-menu slot="dropdown" class="user-dropdown">
           <router-link to="/">
             <el-dropdown-item>
-              Home
+              首页
             </el-dropdown-item>
           </router-link>
-          <a target="_blank" href="https://github.com/PanJiaChen/vue-admin-template/">
-            <el-dropdown-item>Github</el-dropdown-item>
+          <a target="_blank" href="https://github.com/SLNotFound/web-management.git">
+            <el-dropdown-item>项目地址</el-dropdown-item>
           </a>
-          <a target="_blank" href="https://panjiachen.github.io/vue-element-admin-site/#/">
-            <el-dropdown-item>Docs</el-dropdown-item>
+          <a target="_blank" @click.prevent="updatePassword">
+            <el-dropdown-item>修改密码</el-dropdown-item>
           </a>
-          <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+
+          <el-dropdown-item @click.native="logout">
+            <span style="display:block;">退出</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
@@ -44,6 +45,11 @@ export default {
     Breadcrumb,
     Hamburger
   },
+  data() {
+    return {
+      showDialog: false
+    }
+  },
   computed: {
     ...mapGetters([
       'sidebar',
@@ -57,7 +63,10 @@ export default {
     },
     async logout() {
       await this.$store.dispatch('user/logout')
-      this.$router.push(`/login?redirect=${this.$route.fullPath}`)
+      this.$router.push(`/login`)
+    },
+    updatePassword() {
+      this.showDialog = true
     }
   }
 }

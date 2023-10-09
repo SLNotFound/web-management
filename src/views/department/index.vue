c3f8cc5adcde5610096f37cab799ddbfedd9bf67
sunlei <1255193087@qq.com>
2023-10-09 17:25:11 +0800

    作用域插槽

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,7 +1,24 @@
 <template>
   <div class="container">
     <div class="app-container">
-      <el-tree default-expand-all :data="depts" :props="defaultProps" />
+      <el-tree default-expand-all :data="depts" :props="defaultProps">
+        <template v-slot="{ data }">
+          <el-row style="width:100%;height:40px" type="flex" justify="space-between" align="middle">
+            <el-col>{{ data.name }}</el-col>
+            <el-col :span="4">
+              <span class="tree-manager">{{ data.managerName }}</span>
+              <el-dropdown>
+                <span class="el-dropdown-link">操作<i class="el-icon-arrow-down el-icon--right" /></span>
+                <el-dropdown-menu slot="dropdown">
+                  <el-dropdown-item command="add">添加子部门</el-dropdown-item>
+                  <el-dropdown-item command="edit">编辑部门</el-dropdown-item>
+                  <el-dropdown-item command="del">删除</el-dropdown-item>
+                </el-dropdown-menu>
+              </el-dropdown>
+            </el-col>
+          </el-row>
+        </template>
+      </el-tree>
     </div>
   </div>
 </template>
@@ -13,10 +30,11 @@ export default {
     return {
       depts: [{
         name: '天下第一',
+        managerName: '管理员',
         children: [
-          { name: '总经办' },
-          { name: '行政中心' },
-          { name: '财务部' }
+          { name: '总经办', managerName: '张三' },
+          { name: '行政中心', managerName: '李四' },
+          { name: '财务部', managerName: '王五' }
         ]
       }],
       defaultProps: {
@@ -32,5 +50,10 @@ export default {
 .app-container {
   padding: 30px 140px;
   font-size: 14px;
+}
+.tree-manager {
+  width: 50px;
+  display: inline-block;
+  margin: 10px;
 }
 </style>

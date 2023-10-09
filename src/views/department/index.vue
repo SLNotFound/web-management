d603fa1c48a940318d02305467f878b78c93db35
sunlei <1255193087@qq.com>
2023-10-09 16:56:15 +0800

    树型组件的应用

--- src/views/department/index.vue
+++ src/views/department/index.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="container">
     <div class="app-container">
-      组织架构
+      <el-tree default-expand-all :data="depts" :props="defaultProps" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Department'
+  name: 'Department',
+  data() {
+    return {
+      depts: [{
+        name: '天下第一',
+        children: [
+          { name: '总经办' },
+          { name: '行政中心' },
+          { name: '财务部' }
+        ]
+      }],
+      defaultProps: {
+        label: 'name',
+        children: 'children'
+      }
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.app-container {
+  padding: 30px 140px;
+  font-size: 14px;
+}
 </style>

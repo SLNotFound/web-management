18869d87b9a0f75fa2bf0fdd8c99cf1a79688cf4
sunlei <1255193087@qq.com>
2023-10-10 15:01:01 +0800

    部门名称校验

--- src/views/department/components/add-dept.vue
+++ src/views/department/components/add-dept.vue
@@ -27,6 +27,7 @@
 </template>
 
 <script>
+import { getDepartment } from '@/api/department'
 export default {
   name: 'AddDept',
   props: {
@@ -48,15 +49,37 @@ export default {
         code: [{ required: true, message: '部门编码不能为空', trigger: 'blur' },
           {
             min: 2, max: 10, message: '部门编码的长度为2-10个字符', trigger: 'blur'
+          },
+          {
+            trigger: 'blur',
+            validator: async(rule, value, callback) => {
+              const result = await getDepartment()
+              if (result.some(item => item.code === value)) {
+                callback(new Error('部门中已经有该编码了'))
+              } else {
+                callback()
+              }
+            }
           }
         ],
         introduce: [{ required: true, message: '部门介绍不能为空', trigger: 'blur' }, {
           min: 1, max: 100, message: '部门介绍的长度为1-100个字符', trigger: 'blur'
         }],
         managerId: [{ required: true, message: '部门负责人不能为空', trigger: 'blur' }],
-        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' }, {
-          min: 2, max: 10, message: '部门名称的长度为2-10个字符', trigger: 'blur'
-        }]
+        name: [{ required: true, message: '部门名称不能为空', trigger: 'blur' },
+          { min: 2, max: 10, message: '部门名称的长度为2-10个字符', trigger: 'blur' },
+          {
+            trigger: 'blur',
+            validator: async(rule, value, callback) => {
+              const result = await getDepartment()
+              if (result.some(item => item.name === value)) {
+                callback(new Error('部门中已经有该名称了'))
+              } else {
+                callback()
+              }
+            }
+          }
+        ]
       }
     }
   },

b13dfdefd0889695f5dd990ec4f30779ed93e0ba
sunlei <1255193087@qq.com>
2023-10-12 09:47:34 +0800

    角色管理页面

--- src/views/role/index.vue
+++ src/views/role/index.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="container">
     <div class="app-container">
-      角色管理
+      <!-- 角色管理 -->
+      <div class="role-operate">
+        <el-button size="mini" type="primary">添加角色</el-button>
+      </div>
+      <el-table>
+        <el-table-column align="center" width="200" label="角色" />
+        <el-table-column align="center" width="200" label="启用" />
+        <el-table-column align="center" label="描述" />
+        <el-table-column align="center" label="操作" />
+      </el-table>
+      <el-row type="flex" style="height:60px" align="middle" justify="end">
+        <el-pagination layout="prev, pager, next" />
+      </el-row>
     </div>
   </div>
 </template>
@@ -10,3 +22,9 @@ export default {
   name: 'Role'
 }
 </script>
+
+<style scoped>
+.role-operate {
+  padding: 10px;
+}
+</style>

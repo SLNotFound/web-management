0f20db9a87b534fab63db63eba1931225121fe6d
sunlei <1255193087@qq.com>
2023-10-19 11:41:45 +0800

    级联组件双向绑定

--- src/views/employee/components/select-tree.vue
+++ src/views/employee/components/select-tree.vue
@@ -1,9 +1,11 @@
 <template>
   <el-cascader
+    :value="value"
     size="mini"
     :options="treeData"
     :props="props"
     separator="-"
+    @change="changeValue"
   />
 </template>
 
@@ -11,6 +13,12 @@
 import { getDepartment } from '@/api/department'
 import { transListToTreeData } from '@/utils'
 export default {
+  props: {
+    value: {
+      type: Number,
+      default: null
+    }
+  },
   data() {
     return {
       treeData: [],
@@ -26,6 +34,13 @@ export default {
   methods: {
     async getDepartment() {
       this.treeData = transListToTreeData(await getDepartment(), 0)
+    },
+    changeValue(list) {
+      if (list.length > 0) {
+        this.$emit('input', list[list.length - 1])
+      } else {
+        this.$emit('input', null)
+      }
     }
   }
 }
